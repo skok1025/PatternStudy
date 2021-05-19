6040abc656f9c6fc4ae603cd7a2474506d76a146
김석현 <@DESKTOP-82CIIBK>
2021-05-19 12:02:53 +0900

    singleton 패턴

---
+++ src/singleton_pattern/Main.java
@@ -0,0 +1,18 @@
+package singleton_pattern;
+
+public class Main {
+	public static void main(String[] args) {
+		SystemSpeaker speaker1 = SystemSpeaker.getInstance();
+		SystemSpeaker speaker2 = SystemSpeaker.getInstance();
+
+		// 5, 5
+		System.out.println(speaker1.getVolume());
+		System.out.println(speaker2.getVolume());
+		
+		speaker1.setVolume(11);
+		System.out.println(speaker1.getVolume());
+		System.out.println(speaker2.getVolume());
+		
+	}
+
+}
---
+++ src/singleton_pattern/SystemSpeaker.java
@@ -0,0 +1,31 @@
+package singleton_pattern;
+
+public class SystemSpeaker {
+	private static SystemSpeaker instance;
+	private int volume;
+	
+	private SystemSpeaker() {
+		volume = 5;
+	}
+	
+	
+	public static SystemSpeaker getInstance() {
+		if (instance == null) {
+			instance = new SystemSpeaker();
+			System.out.println("새로 생성된 인스턴스");
+		} else {
+			System.out.println("이미 생성된 인스턴스");
+		}
+		
+		return instance;
+	}
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+}
